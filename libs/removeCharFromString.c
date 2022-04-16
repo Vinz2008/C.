1418a931e2f0500f33917300af965510495b5be4
Vincent Bidard de la Noë <vincentbidarddelanoe@gmail.com>
2022-04-16 19:06:58 +0200

    fixed removecharfromstring

--- libs/removeCharFromString.c
+++ libs/removeCharFromString.c
@@ -1,1 +1,18 @@
-extern char *removeCharFromString(char charToRemove, char *str);
+#include "removeCharFromString.h"
+#include <stdio.h>
+#include <string.h>
+char *removeCharFromString(char charToRemove, char *str){
+int i,j;
+i = 0;
+while(i<strlen(str))
+{
+    if (str[i]==charToRemove)
+    {
+        for (j=i; j<strlen(str); j++) {
+            str[j]=str[j+1];
+        }
+    }
+    else i++;
+}
+return str;
+}
