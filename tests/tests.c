cbccbf302ee4dfd87a0b7b78ded65f732ffa31dd
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-12-19 01:32:35 +0100

    started adding tests

---
+++ tests/tests.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NB_FILES 6
+
+int main(){
+	char exe[] = "../cpoint";
+	char* cmd = NULL;
+	FILE* cmdf;
+	for (int i = 0; i < NB_FILES; i++){
+	int size = strlen(exe) + 1 + 7 + 1 + 7 + 1;
+	cmd = malloc(size * sizeof(char));
+	snprintf(cmd, size, "%s ../test%d.cpoint", exe, i+1);
+	printf("cmd : %s\n", cmd);
+	cmdf = popen(cmd, "r");
+	char* out = malloc(1000 * sizeof(char));
+	//fscanf(cmdf, "%s", out);
+	fread(out, 1, 1000, cmdf);
+	printf("out : %s\n", out);
+	pclose(cmdf);
+	free(cmd);
+	free(out);
+	}
+	return 0;
+}
