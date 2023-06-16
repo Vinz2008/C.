dccc54dcc949e845928b28d62a2865c7145af7a7
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-06-16 15:55:56 +0200

    work on files in standard library

--- std/c_api/file.c
+++ std/c_api/file.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 
+#ifdef _WIN32
+#define write(fd, buffer, count) _write(fb, buffer, count)
+#endif
+
 int cwritefile(int fd, const char* buf){
     return write(fd, buf, strlen(buf));
 }
