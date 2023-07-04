127ba7fc4ef4c8e98dc3c47f0ac802a66bded1f6
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-07-04 18:51:40 +0200

    add gitignore to new project created by cpoint-build

--- build/src/project_creator.cpp
+++ build/src/project_creator.cpp
@@ -11,6 +11,12 @@ func main(){
 )";
 
 
+std::string gitignore_template = R"(*.out
+*.a
+*.ll
+*.o
+)";
+
 // need to add type variable
 std::string build_toml_template = R"([project]
 name = "test"
@@ -18,16 +24,20 @@ homepage = "https://github.com/test"
 license = "GPL"
 )";
 
+
 void createProject(std::string type, std::string project_name){
     std::string folder = "./" + project_name + "/";
     fs::create_directories(folder);
     std::string code_folder = folder + "src/";
     fs::create_directories(code_folder);
     std::ofstream config_file(folder + "build.toml");
+    std::ofstream gitignore_file(folder + ".gitignore");
     std::ofstream main_file(code_folder + "main.cpoint");
     main_file << file_main_template;
+    gitignore_file << gitignore_template;
     config_file << build_toml_template;
     config_file << "type = \"" << type << "\"" << std::endl;
     config_file.close();
     main_file.close();
+    gitignore_file.close();
 }
