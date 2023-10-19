315c48bc053aeff2f4ca1e37e4bd49fb06c32c40
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-10-19 20:33:00 +0200

    work on lto

---
+++ src/lto.cpp
@@ -0,0 +1,16 @@
+#include "llvm/Transforms/IPO/ThinLTOBitcodeWriter.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+
+using namespace llvm;
+
+/*
+void getBitcodeData(Module mod){
+    std::string str;
+    legacy::PassManager pass;
+    raw_string_ostream OS(str);
+    
+    pass.add(ThinLTOBitcodeWriterPass(OS, nullptr));
+    pass.run(Mod);
+}
+*/
