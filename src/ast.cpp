255b3a2104dcc2176d56235d9ce71c44da831472
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-10-30 17:15:34 +0100

    remove useless comments

--- src/ast.cpp
+++ src/ast.cpp
@@ -743,44 +743,15 @@ std::unique_ptr<ExprAST> ParseMacroCall(){
         return get_filename_tok();
     } else if (function_name == "expect"){
         return generate_expect(ArgsMacro);
-        /*std::vector<std::unique_ptr<ExprAST>> Args;
-        std::vector<std::unique_ptr<ExprAST>> ArgsEmpty;
-        if (ArgsMacro.size() != 1){
-            return LogError("Wrong number of args for %s macro function call : expected %d, got %d", "expect", 1, ArgsMacro.size());
-        }
-        Args.push_back(ArgsMacro.at(0)->clone());
-        Args.push_back(get_filename_tok());
-        Args.push_back(std::make_unique<CallExprAST>(emptyLoc, "cpoint_internal_get_function_name", std::move(ArgsEmpty), Cpoint_Type(double_type)));
-        Args.push_back(stringify_macro(ArgsMacro.at(0)->clone()));
-        return std::make_unique<CallExprAST>(emptyLoc, "expectxplusexpr", std::move(Args), Cpoint_Type(double_type));*/
     } else if (function_name == "panic"){
         return generate_panic(ArgsMacro);
-        /*std::vector<std::unique_ptr<ExprAST>> Args;
-        std::vector<std::unique_ptr<ExprAST>> ArgsEmpty;
-        if (ArgsMacro.size() != 1){
-            return LogError("Wrong number of args for %s macro function call : expected %d, got %d", "expect", 1, ArgsMacro.size());
-        }
-        Args.push_back(ArgsMacro.at(0)->clone());
-        Args.push_back(get_filename_tok());
-        Args.push_back(std::make_unique<CallExprAST>(emptyLoc, "cpoint_internal_get_function_name", std::move(ArgsEmpty), Cpoint_Type(double_type)));
-        return std::make_unique<CallExprAST>(emptyLoc, "panicx", std::move(Args), Cpoint_Type(double_type));*/
     } else if (function_name == "stringify"){
         if (ArgsMacro.size() != 1){
             return LogError("Wrong number of args for %s macro function call : expected %d, got %d", "stringify", 1, ArgsMacro.size());
-        } 
+        }
         return stringify_macro(std::move(ArgsMacro.at(0)));
     } else if (function_name == "concat"){
         return generate_concat_macro(ArgsMacro);
-        /*std::string concatenated_str = "";
-        for (int i = 0; i < ArgsMacro.size(); i++){
-            if (dynamic_cast<StringExprAST*>(ArgsMacro.at(i).get())){
-                auto s = dynamic_cast<StringExprAST*>(ArgsMacro.at(i).get());
-                concatenated_str += s->str;
-            } else {
-                concatenated_str += ArgsMacro.at(i)->to_string();
-            }
-        }
-        return std::make_unique<StringExprAST>(concatenated_str);*/
     } else if (function_name == "line"){
         return std::make_unique<StringExprAST>(Comp_context->line);
     } else if (function_name == "line_nb"){
@@ -789,70 +760,16 @@ std::unique_ptr<ExprAST> ParseMacroCall(){
         return std::make_unique<NumberExprAST>((double)Comp_context->curloc.col_nb);
     } else if (function_name == "time"){
         return generate_time_macro();
-        /*std::string time_str;
-        char* time_str_c = (char*)malloc(sizeof(char) * 21);
-        std::time_t current_time;
-        std::tm* time_info;
-        time(&current_time);
-        time_info = localtime(&current_time);
-        strftime(time_str_c, 21, "%H:%M:%S", time_info);
-        time_str = time_str_c;
-        free(time_str_c);
-        return std::make_unique<StringExprAST>(time_str);*/
     } else if (function_name == "env"){
         return generate_env_macro(ArgsMacro);
-        /*if (ArgsMacro.size() != 1){
-            return LogError("Wrong number of args for %s macro function call : expected %d, got %d", "env", 1, ArgsMacro.size());
-        }
-        StringExprAST* str = nullptr;
-        if (dynamic_cast<StringExprAST*>(ArgsMacro.at(0).get())){
-            str = dynamic_cast<StringExprAST*>(ArgsMacro.at(0).get());
-        } else {
-            return LogError("Wrong type of args for %s macro function call : expected a string", "env");
-        }
-        std::string env_name = str->str;
-        auto path_val = std::getenv(env_name.c_str());
-        if (path_val == nullptr){
-            return LogError("Env variable %s doesn't exist for %s macro function call", env_name.c_str(), "env");
-        }
-        return std::make_unique<StringExprAST>((std::string)path_val);*/
     } else if (function_name == "asm"){
         return generate_asm_macro(ArgsMacro);
-        /*if (ArgsMacro.size() != 1){
-            return LogError("Wrong number of args for %s macro function call : expected %d, got %d", "asm", 1, ArgsMacro.size());
-        }
-        StringExprAST* str = nullptr;
-        if (dynamic_cast<StringExprAST*>(ArgsMacro.at(0).get())){
-            str = dynamic_cast<StringExprAST*>(ArgsMacro.at(0).get());
-        } else {
-            return LogError("Wrong type of args for %s macro function call : expected a string", "asm");
-        }
-        std::string assembly_code = str->str;
-        return std::make_unique<AsmExprAST>(assembly_code);*/
     } else if (function_name == "arch"){
         return std::make_unique<StringExprAST>(context->get_variable_value("ARCH"));
     } else if (function_name == "todo") {
         return generate_todo_macro(ArgsMacro);
-        /*if (ArgsMacro.size() > 2){
-            return LogError("Wrong number of args for %s macro function call : expected less or equal to %d, got %d", "todo", 2, ArgsMacro.size());
-        }
-        std::vector<std::unique_ptr<ExprAST>> Args;
-        std::string basic_message = "not implemented";
-        if (ArgsMacro.empty()){
-            Args.push_back(std::make_unique<StringExprAST>(basic_message + "\n"));
-        } else {
-            //Args.push_back(std::make_unique<StringExprAST>(basic_message + " : "));
-            return LogError("You can't use the todo macro with args for now"); // TODO
-        }
-        return std::make_unique<CallExprAST>(emptyLoc, "printf", std::move(Args), Cpoint_Type());*/
     } else if (function_name == "dbg"){
         return generate_dbg_macro(ArgsMacro);
-        /*if (ArgsMacro.size() != 1){
-            return LogError("Wrong number of args for %s macro function call : expected %d, got %d", "dbg", 1, ArgsMacro.size());
-        }
-        std::vector<std::unique_ptr<ExprAST>> Args;
-        Args.push_back(std::move(ArgsMacro.at(0)));
-        return std::make_unique<CallExprAST>(emptyLoc, "cpoint_internal_dbg", std::move(Args), Cpoint_Type());*/
     }
     return LogError("unknown function macro called : %s", function_name.c_str());
 }
